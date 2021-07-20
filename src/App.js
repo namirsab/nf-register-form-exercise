a59d28aa96a27450f2ca77f16467e6eaf44c807a
Namir <namirsab@gmail.com>
2021-07-20 13:58:16 +0200

    Add basic form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import RegisterForm from "./components/RegisterForm";
 export default function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Register to our website!</h1>
+      </header>
       <RegisterForm />
     </div>
   );
